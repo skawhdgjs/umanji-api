2d16eed1bb62a61087cbe835e005fba85a8a8002
unamedkr <unamed.kr@gmail.com>
2016-01-06 02:37:14 +0900

    route 추가

--- config/routes.js
+++ config/routes.js
@@ -13,7 +13,6 @@ export default {
     'GET /v1/photo/id'                : 'PhotoController.get',
 
     'POST /v1/channels/spot'          : 'ChannelController.createSpot',
-    'POST /v1/channels/id'            : 'ChannelController.create',
 
     'GET /v1/channels'                : 'ChannelController.find',
     'GET /v1/channels/id'             : 'ChannelController.get',
@@ -21,11 +20,18 @@ export default {
     'GET /v1/channels/posts'          : 'ChannelController.findPosts',
 
     'GET /v1/channels/point'          : 'ChannelController.getByPoint',
+
+
     'GET /v1/channels/links'          : 'ChannelController.getLevelLinks',
     'GET /v1/channels/id/links'       : 'ChannelController.getLinks',
     'GET /v1/channels/id/posts'       : 'ChannelController.getLinks',
     'GET /v1/channels/id/members'     : 'ChannelController.getLinks',
     'GET /v1/channels/id/communities' : 'ChannelController.getLinks',
+
+    'POST /v1/channels/id/link'       : 'ChannelController.create',
+    'POST /v1/channels/id/post'       : 'ChannelController.create',
+    'POST /v1/channels/id/member'     : 'ChannelController.create',
+    'POST /v1/channels/id/community'  : 'ChannelController.create',
 
     'GET /v1/users/id/links'          : 'UserController.getLinks',
   }
